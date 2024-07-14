c9c00d9f51b5ee9b297ddfe4cc9b3940a506f8d3
NQTrung08 <nqtrung0810@gmail.com>
2024-07-15 01:15:14 +0700

    bug: fix router getall Teacher

--- src/Routes/teacherRouter.js
+++ src/Routes/teacherRouter.js
@@ -2,8 +2,8 @@ const router = require("express").Router()
 const middlewareControler = require('../MiddleWare/middlewareControler')
 const TeacherControler = require("../Controller/user/TeacherController")
 
+router.get('/getAll',middlewareControler.verifyTokenIsAdmin, TeacherControler.getAll)
 router.get('/:teacherId', middlewareControler.verifyTokenIsAdminOrGV, TeacherControler.getTeacher)
-router.get('/get-all',middlewareControler.verifyTokenIsAdmin, TeacherControler.getAll)
 router.post('/create-teacher',middlewareControler.verifyTokenIsAdmin, TeacherControler.createTeacher)
 
 module.exports = router
